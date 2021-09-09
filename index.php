bc5db6a101d077c97e82d678bb18db29f72df1ec
salim <salimtison@gmail.com>
2021-09-09 11:51:26 +0200

    html and css style

--- index.php
+++ index.php
@@ -5,10 +5,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <script defer src="public/js/index.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+    <title>Todo</title>
 </head>
 
 <body>
+    <div class="container">
+        <header class="logo">Suits Todo</header>
+        <div class="content">
+            <h1 class="todo-container">Todo List</h1>
+            <div class="todo-form"></div>
+            <div class="todo-list"></div>
+        </div>
+        <footer>2021 / &copy Tous droits réservés</footer>
+    </div>
 
 </body>
 
