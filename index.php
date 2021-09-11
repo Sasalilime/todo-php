ffaed91e2e4c624e83d57694a0d7ee732748d2b7
sasalilime <salimtison@gmail.com>
2021-09-11 13:13:29 +0200

    css form

--- index.php
+++ index.php
@@ -10,8 +10,14 @@
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
-            <h1 class="todo-container">Todo List</h1>
-            <div class="todo-form"></div>
+            <div class="todo-container">
+                <h1>Todo List</h1>
+                <form class="todo-form" method="post" action="/">
+                    <input type="text">
+                    <button class="btn btn-primary">Ajouter</button>
+                </form>
+            </div>
+
             <div class="todo-list"></div>
         </div>
         <?php require_once 'includes/footer.php' ?>
