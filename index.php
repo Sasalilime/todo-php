b09dafd522cd759066bd0fbc6687289dbac6ed25
sasalilime <salimtison@gmail.com>
2021-09-11 13:41:42 +0200

    pb error

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+
+const ERROR_REQUIRED = 'Veuillez indiquer une todo.';
+const ERROR_TOO_SHORT = 'Veuillez indiquer au moins 5 caractères.';
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $todo = $_POST['todo'] ?? '';
+
+    if (!$todo) {
+        $error = ERROR_REQUIRED;
+    } else if (mb_strlen($todo) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -16,9 +37,12 @@
                     <input type="text">
                     <button class="btn btn-primary">Ajouter</button>
                 </form>
+                <?php if ($error) : ?>
+                    <p class="text-danger"><?= $error ?></p>
+                <?php endif; ?>
+                <div class="todo-list"></div>
             </div>
 
-            <div class="todo-list"></div>
         </div>
         <?php require_once 'includes/footer.php' ?>
 
